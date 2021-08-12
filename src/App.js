f5ea48725730707d77edad33d794fa1c86873319
Evi Fara <faraparaskevi@gmail.com>
2021-08-12 12:10:02 +0200

    Import BrowserRouter as Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return <div className="App">Hello World!</div>;
