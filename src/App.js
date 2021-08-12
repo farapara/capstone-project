c94670d06699fae6b41c585c897e169506c41436
Evi Fara <faraparaskevi@gmail.com>
2021-08-12 12:55:36 +0200

    Add Route, Switch to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import "./App.css";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, NavLink } from "react-router-dom";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <Switch>
+        <Route path="/places">places</Route>
+      </Switch>
+      <Switch>
+        <Route path="/profile">profile</Route>
+      </Switch>
+
+      <Switch>
+        <Route exact path="/">
+          HOME
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
