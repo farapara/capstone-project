7ac83be10f562a129f1a7412efa1509bf7a39c8d
Evi Fara <faraparaskevi@gmail.com>
2021-08-15 09:47:45 +0200

    Add icons in the footer

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,19 @@
 import "./Navigation.css";
 import { NavLink } from "react-router-dom";
+import { FaRegPlusSquare, FaRegImages, FaMapMarked } from "react-icons/fa";
 
 export default function Navigation() {
   return (
     <section>
       <nav className="Nav">
         <NavLink className="Navlink" to="/profile">
-          profile
+          <FaRegPlusSquare />
         </NavLink>
         <NavLink className="Navlink" to="/places">
-          places
+          <FaRegImages />
         </NavLink>
         <NavLink className="Navlink" to="/map">
-          map
+          <FaMapMarked />
         </NavLink>
       </nav>
     </section>
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,6 +3,7 @@ import Navigation from "./components/Navigation";
 export default function Profile() {
   return (
     <section>
+      <div className="App"></div>
       <main className="Main">
         <div>create profile page</div>
       </main>
