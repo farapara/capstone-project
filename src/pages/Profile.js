fbb034343784cf29834ec4a2f98e8db3db8093ee
Evi Fara <faraparaskevi@gmail.com>
2021-08-15 08:59:58 +0200

    Add Profile.js page

---
+++ src/pages/Profile.js
@@ -0,0 +1,12 @@
+import Navigation from "./components/Navigation";
+
+export default function Profile() {
+  return (
+    <section>
+      <div>create profile page</div>
+      <footer className="Footer">
+        <Navigation />
+      </footer>
+    </section>
+  );
+}
