3fca409a8c8c8c32cc99c60e0e042cc57390b583
Evi Fara <faraparaskevi@gmail.com>
2021-08-15 22:27:56 +0200

    Add Upload to the Form and chande imputs to textareas

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -4,29 +4,44 @@ export default function Profile() {
   return (
     <section>
       <h2>you can create your profile here!</h2>
-      <div className="form">
+      <div className="Form">
         <form className="ProfileForm">
-          <input
+          <p>Name:</p>
+          <textarea
             name=""
             id=""
             type="text"
             placeholder="enter a name"
+            maxLength="10"
             required
-          />
-          <input
+          ></textarea>
+          <p>Age:</p>
+          <textarea
             name=""
             id=""
             type="number"
             min="0"
             placeholder="enter an age"
             required
+          ></textarea>
+          <p>Bio:</p>
+          <textarea
+            name=""
+            id=""
+            type="text"
+            placeholder="add bio here..."
+            maxlenght="200"
+            required
           />
-          <input name="" id="" type="text" placeholder="add bio" required />
+          <div value="">200</div>
         </form>
       </div>
+      <label className="Upload" for="upload"></label>
+      <input class name="" id="" type="file" required />
+
       <div className="FormButtons">
-        <button className="submit">save</button>
-        <button className="cancel">cancel</button>
+        <button className="Submit">save</button>
+        <button className="Cancel">cancel</button>
       </div>
     </section>
   );
