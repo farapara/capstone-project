407f1afe4add02f8053932ae6be8efefd9bd51e0
Evi Fara <faraparaskevi@gmail.com>
2021-08-15 18:04:03 +0200

    Add content to Profile.js and create Profile.css

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import "./App.css";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <main className="Main"></main>
+      <main className="Main">
+        <Profile />
+      </main>
       <Footer />
     </div>
   );
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,16 +1,33 @@
-import Navigation from "./components/Navigation";
+import "./Profile.css";
 
 export default function Profile() {
   return (
     <section>
-      <div className="App"></div>
-      <main className="Main">
-        <div>create profile page</div>
-      </main>
-
-      <footer className="Footer">
-        <Navigation />
-      </footer>
+      <h2>you can create your profile here!</h2>
+      <div className="form">
+        <form className="ProfileForm">
+          <input
+            name=""
+            id=""
+            type="text"
+            placeholder="enter a name"
+            required
+          />
+          <input
+            name=""
+            id=""
+            type="number"
+            min="0"
+            placeholder="enter an age"
+            required
+          />
+          <input name="" id="" type="text" placeholder="add bio" required />
+        </form>
+      </div>
+      <div className="FormButtons">
+        <button className="submit">save</button>
+        <button className="cancel">cancel</button>
+      </div>
     </section>
   );
 }
