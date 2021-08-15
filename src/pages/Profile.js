8218f08c741fac18d2a76062007ab3e6cae9e1f5
Evi Fara <faraparaskevi@gmail.com>
2021-08-15 09:30:58 +0200

    Add react-icons

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,7 +3,10 @@ import Navigation from "./components/Navigation";
 export default function Profile() {
   return (
     <section>
-      <div>create profile page</div>
+      <main className="Main">
+        <div>create profile page</div>
+      </main>
+
       <footer className="Footer">
         <Navigation />
       </footer>
