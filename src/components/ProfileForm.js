e5743bdfb6e232df3c4fc41e4bdeb66590e997e8
Evi Fara <faraparaskevi@gmail.com>
2021-08-16 11:15:37 +0200

    Remove the form to its own componenent

---
+++ src/components/ProfileForm.js
@@ -0,0 +1,46 @@
+import "./ProfileForm.css";
+
+export default function ProfileForm() {
+  return (
+    <div className="Form-Wrapper">
+      <form className="ProfileForm">
+        <p>Name:</p>
+        <textarea
+          name="userName"
+          id="userName"
+          type="text"
+          placeholder="enter a name"
+          maxLength="10"
+          required
+        ></textarea>
+        <p>Age:</p>
+        <textarea
+          name="userAge"
+          id="userAge"
+          type="number"
+          min="0"
+          placeholder="enter an age"
+          required
+        ></textarea>
+        <p>Bio:</p>
+        <textarea
+          name="userBio"
+          id="userBio"
+          type="text"
+          placeholder="add bio here..."
+          maxlenght="200"
+          required
+        />
+        <div value="">200</div>
+
+        <label className="Upload" for="upload"></label>
+        <input class name="userPicture" id="userPicture" type="file" required />
+
+        <div className="FormButtons">
+          <button className="Submit">save</button>
+          <button className="Cancel">cancel</button>
+        </div>
+      </form>
+    </div>
+  );
+}
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,55 +1,11 @@
+import ProfileForm from "../components/ProfileForm";
 import "./Profile.css";
 
 export default function Profile() {
   return (
     <section>
       <h2>you can create your profile here!</h2>
-      <div className="Form-Wrapper">
-        <form className="ProfileForm">
-          <p>Name:</p>
-          <textarea
-            name="userName"
-            id="userName"
-            type="text"
-            placeholder="enter a name"
-            maxLength="10"
-            required
-          ></textarea>
-          <p>Age:</p>
-          <textarea
-            name="userAge"
-            id="userAge"
-            type="number"
-            min="0"
-            placeholder="enter an age"
-            required
-          ></textarea>
-          <p>Bio:</p>
-          <textarea
-            name="userBio"
-            id="userBio"
-            type="text"
-            placeholder="add bio here..."
-            maxlenght="200"
-            required
-          />
-          <div value="">200</div>
-
-          <label className="Upload" for="upload"></label>
-          <input
-            class
-            name="userPicture"
-            id="userPicture"
-            type="file"
-            required
-          />
-
-          <div className="FormButtons">
-            <button className="Submit">save</button>
-            <button className="Cancel">cancel</button>
-          </div>
-        </form>
-      </div>
+      <ProfileForm />
     </section>
   );
 }
