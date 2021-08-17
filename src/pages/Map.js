6918a3e598e02fb838db90487275234724a4fbc9
Evi Fara <faraparaskevi@gmail.com>
2021-08-17 14:32:02 +0200

    Create an .env file for storing the token link

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,10 +1,18 @@
 import "./Map.css";
 import ReactMapGl from "react-map-gl";
+import { useState } from "react";
 
 export default function Map() {
+  const [viewport, setViewport] = useState({
+    latitude: 42.4211,
+    longitude: 24.345,
+    width: "590px",
+    height: "450px",
+    zoom: 0,
+  });
   return (
     <div>
-      <ReactMapGl>pins here</ReactMapGl>
+      <ReactMapGl {...viewport}>markers here</ReactMapGl>
     </div>
   );
 }
