383c3d189a1be49e1b9da5b9cfd88b3c3f83e547
Evi Fara <faraparaskevi@gmail.com>
2021-08-17 18:05:22 +0200

    Test that map works

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -4,15 +4,21 @@ import { useState } from "react";
 
 export default function Map() {
   const [viewport, setViewport] = useState({
-    latitude: 42.4211,
-    longitude: 24.345,
-    width: "590px",
-    height: "450px",
-    zoom: 0,
+    latitude: 48.181984,
+    longtitude: 11.552184,
+    width: "100vw",
+    height: "100vh",
+    zoom: 10,
   });
   return (
     <div>
-      <ReactMapGl {...viewport}>markers here</ReactMapGl>
+      <ReactMapGl
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_KEY}
+        onViewportChange={(viewport) => {
+          setViewport(viewport);
+        }}
+      ></ReactMapGl>
     </div>
   );
 }
