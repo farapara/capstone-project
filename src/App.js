784e70f10792c11425a02022052f4c06d2cdfaf3
Evi Fara <faraparaskevi@gmail.com>
2021-08-19 12:27:49 +0200

    Change the navlinks icons, add homepage and change the Route paths

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Homepage from ".//pages/Homepage";
 import Profile from ".//pages/Profile";
-import Places from ".//pages/Places";
+import Pins from ".//pages/Pins";
 import Map from ".//pages/Map";
-import { Switch, Route, Redirect } from "react-router";
+import { Switch, Route } from "react-router";
 
 function App() {
   return (
@@ -15,14 +16,14 @@ function App() {
           <Route path="/profile">
             <Profile />
           </Route>
-          <Route path="/places">
-            <Places />
-          </Route>
           <Route path="/map">
             <Map />
           </Route>
+          <Route path="/add">
+            <Pins />
+          </Route>
           <Route exact path="/">
-            <Redirect to="/profile" />
+            <Homepage />
           </Route>
           <Route path="*">
             <p>Page Not Found</p>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,27 @@
 import "./Footer.css";
 import { NavLink } from "react-router-dom";
-import { FaRegPlusSquare, FaRegImages, FaMapMarkedAlt } from "react-icons/fa";
+import { FiMap } from "react-icons/fi";
+import { BiLocationPlus, BiHomeAlt } from "react-icons/bi";
+import { CgProfile } from "react-icons/cg";
 
 export default function Footer() {
   return (
     <nav className="Nav">
+      <NavLink className="Navlink" to="/" activeClassName="Navlink--active">
+        <BiHomeAlt />
+      </NavLink>
       <NavLink
         className="Navlink"
         to="/profile"
         activeClassName="Navlink--active"
       >
-        <FaRegPlusSquare />
-      </NavLink>
-      <NavLink
-        className="Navlink"
-        to="/places"
-        activeClassName="Navlink--active"
-      >
-        <FaRegImages />
+        <CgProfile />
       </NavLink>
       <NavLink className="Navlink" to="/map" activeClassName="Navlink--active">
-        <FaMapMarkedAlt />
+        <FiMap />
+      </NavLink>
+      <NavLink className="Navlink" to="/add" activeClassName="Navlink--active">
+        <BiLocationPlus />
       </NavLink>
     </nav>
   );
