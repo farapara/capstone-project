7b04e72f66567a4abc25b69e5b61a3686f895345
Evi Fara <faraparaskevi@gmail.com>
2021-08-29 17:31:58 +0200

    Create pins.json file, render json file on map page

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -5,11 +5,12 @@ import ReactMapGl, {
   GeolocateControl,
   NavigationControl,
 } from "react-map-gl";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { FaMapMarkerAlt } from "react-icons/fa";
 
 export default function Map() {
+  const [pins, setPins] = useState({ pins: [] });
   const [viewport, setViewport] = useState({
     latitude: 42.123,
     longitude: 10.123,
@@ -28,54 +29,68 @@ export default function Map() {
     right: 10,
     top: 10,
   };
-  function renderMarkers() {
-    return (
-      <div className="Map">
-        <ReactMapGl
-          {...viewport}
-          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_KEY}
-          mapStyle="mapbox://styles/farapara/cksh5t2bc6to818nta74mtluc"
-          onViewportChange={(viewport) => {
-            setViewport(viewport);
-          }}
-        >
-          <Marker
-            longitude={23.72611}
-            latitude={37.970833}
-            offsetLeft={-20}
-            offsetTop={-10}
-          >
-            <FaMapMarkerAlt
-              style={{ fontSize: viewport.zoom * 7, color: "#e8abb9" }}
-            />
-          </Marker>
-          <Popup
-            longitude={23.72611}
-            latitude={37.970833}
-            closeButton={true}
-            closeOnClick={false}
-            anchor="left"
-          >
-            <div className="PinCard">
-              <label className="PinCardLabel">Location</label>
-              <p className="PinCardDesc"></p>
-              <label className="PinCardLabel">Date</label>
-              <p className="PinCardDesc"></p>
-              <label className="PinCardLabel">Rating</label>
-              <div className="PinCardDesc">5</div>
-              <label className="PinCardLabel">Notes</label>
-              <p className="PinCardDesc">Happy place</p>
-            </div>
-          </Popup>
-          <NavigationControl style={navControlStyle} />
-          <GeolocateControl
-            style={geolocateStyle}
-            positionOptions={{ enableHighAccuracy: true }}
-            trackUserLocation={true}
-          />
-        </ReactMapGl>
-      </div>
-    );
-  }
-  return <div>{renderMarkers()} </div>;
+
+  useEffect(() => {
+    const url = "/api/pins.json";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setPins(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div className="Map">
+      <ReactMapGl
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_KEY}
+        mapStyle="mapbox://styles/farapara/cksh5t2bc6to818nta74mtluc"
+        onViewportChange={(viewport) => {
+          setViewport(viewport);
+        }}
+      >
+        {pins.pins.map((p) => (
+          <>
+            <Marker
+              longitude={p.long}
+              latitude={p.lat}
+              offsetLeft={-20}
+              offsetTop={-10}
+            >
+              <FaMapMarkerAlt
+                style={{ fontSize: viewport.zoom * 7, color: "#e8abb9" }}
+              />
+            </Marker>
+            <Popup
+              longitude={p.long}
+              latitude={p.lat}
+              closeButton={true}
+              closeOnClick={false}
+              anchor="left"
+            >
+              <div className="PinCard">
+                <label className="PinCardLabel">Location</label>
+                <p className="PinCardDesc">{p.location}</p>
+                <label className="PinCardLabel">Date</label>
+                <p className="PinCardDesc">{p.date}</p>
+                <label className="PinCardLabel">Rating</label>
+                <div className="PinCardDesc">{p.rating}</div>
+                <label className="PinCardLabel">Notes</label>
+                <p className="PinCardDesc">{p.notes}</p>
+              </div>
+            </Popup>
+          </>
+        ))}
+        <NavigationControl style={navControlStyle} />
+        <GeolocateControl
+          style={geolocateStyle}
+          positionOptions={{ enableHighAccuracy: true }}
+          trackUserLocation={true}
+        />
+      </ReactMapGl>
+    </div>
+  );
 }
