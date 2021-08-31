2adf088bf78bef66add26115fd1227df09a91a8f
Evi Fara <faraparaskevi@gmail.com>
2021-08-31 09:45:48 +0200

    Restructure my homepage with login/register options

--- src/pages/CreateProfile.js
+++ src/pages/CreateProfile.js
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import "./CreateProfile.css";
-import AddProfileForm from "../components/AddProfileForm";
 
 export default function CreateProfile() {
+  const [currentUser, setCurrentUser] = useState(null);
   return (
     <section>
-      <h2 className="Heading">create your profile here!</h2>
-      <h4>
+      <h2 className="Heading">THE ONE WITH THE MAP</h2>
+      <h4 className="AppDescription">
+        Toys are Taking Vacations and Seeing the World (Without Their Owners) -
+        just for stuffies.
+      </h4>
+      <h5>
         “almost every person I spoke to knew someone who had taken a toy
         travelling.”
-      </h4>
-      <AddProfileForm />
+      </h5>
+      {currentUser ? (
+        <button className="LogoutButton">Log out</button>
+      ) : (
+        <div className="SigninButtons">
+          <button className="LoginButton">Login</button>
+          <button className="RegisterButton">Register</button>
+        </div>
+      )}
     </section>
   );
 }
